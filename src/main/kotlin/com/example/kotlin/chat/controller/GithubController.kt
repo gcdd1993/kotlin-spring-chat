4744ddd6bc616ec742b8a4c4304e8736e5965fd7
gcdd1993 <gcwm99@gmail.com>
2021-11-04 17:06:56 +0800

    其实有两种写法

--- src/main/kotlin/com/example/kotlin/chat/controller/GithubController.kt
+++ src/main/kotlin/com/example/kotlin/chat/controller/GithubController.kt
@@ -14,10 +14,29 @@ class GithubController(
     private val githubApi: GithubApi
 ) {
 
+    // 写法一：使用Mono，手动创建mono上下文，走的是Kotlin Coroutine
+//    @GetMapping("/issues")
+//    fun issues(): Mono<Any?> {
+//        return mono {
+//            githubApi.issues()
+//                .also { print("github issues size: ${(it as List<Any?>).size}") }
+//        }
+//    }
+
+    // 写法二，在方法前加上 suspend ，虽然返回的不是Mono，但是suspend会包装成Mono<*>
     @GetMapping("/issues")
-    suspend fun issues(): Any? {
-        return githubApi.issues()
-            .also { print("github issues size: ${(it as Array<Any?>).size}") }
-    }
+    suspend fun issues(): Any? = githubApi.issues()
+        .also {
+            print("github issues size: ${(it as List<Any?>).size}")
+        }
+
+    // 错误写法，返回的是两次Mono包装，导致出错
+//    @GetMapping("/issues")
+//    suspend fun issues(): Mono<Any?> = mono {
+//        githubApi.issues()
+//            .also {
+//                print("github issues size: ${(it as List<Any?>).size}")
+//            }
+//    }
 
 }
